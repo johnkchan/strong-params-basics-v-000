c28ee6a2b3d601935d77c6029611de5f34f30577
Jonathan Chan <johnkchan252@gmail.com>
2019-01-29 05:00:53 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -27,10 +27,10 @@ class PostsController < ApplicationController
 	  @post = Post.find(params[:id])
 	end
 	
-private
- 
-def post_params
-  params.require(:post).permit(:title, :description)
-end
+  private
+   
+  def post_params
+    params.require(:post).permit(:title, :description)
+  end
 
 end
